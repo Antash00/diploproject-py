937e08951ba987390424eda445f4511b3bac2a6e
Antash00 <antash00@mail.ru>
2021-04-08 16:40:22 +0500

    init commit

---
+++ main.py
@@ -0,0 +1,81 @@
+import requests
+import json
+import time
+start_time = time.time()
+
+vk_api_token = ""
+yandex_api_token = ""
+vk_id = '552934290'
+progressBar = [1, 2, 3, 4, 5, 6, 7, 8]
+
+
+class PhotoUpload:
+    def __init__(self, yandex_token: str, vk_token: str, vkId: str):
+        self.version_vk = '5.52'
+        self.yandex_token = yandex_token
+        self.vk_token = vk_token
+        self.vkId = vkId
+        self.dir_path = ''
+
+    def getUserInfo(self):
+        resp = requests.get('https://api.vk.com/method/users.get',
+                            params={'access_token': self.vk_token, 'user_id': self.vkId, 'v': self.version_vk})
+        return resp.json()
+
+    def getUserPhotos(self):
+        resp = requests.get('https://api.vk.com/method/photos.get',
+                            params={'access_token': self.vk_token, 'user_id': self.vkId, 'extended': 1,
+                                    'album_id': 'profile', 'v': self.version_vk, 'photo_sizes': 1})
+        print("Получение фотографий --- %s seconds ---" % (time.time() - start_time))
+        resp = resp.json()
+        result = resp['response']
+        return result
+
+    def sortUserPhotos(self, photos):
+        sorted_photos = []
+        if 'items' in photos:
+            for elements in photos['items']:
+                el = {'height': 0, 'width': 0}
+                for photo in elements['sizes']:
+                    if el['height'] * el['width'] < photo['height'] * photo['width']:
+                        el = photo
+                        el['name'] = elements['likes']['count']
+                sorted_photos.append(el)
+        sorted_photos = sorted(sorted_photos, key=lambda k: k['width'] * k['height'], reverse=True)
+        print("Сортировка фотографий --- %s seconds ---" % (time.time() - start_time))
+        return sorted_photos
+
+    def createYandexDir(self):
+        if len(self.dir_path) > 0:
+            resp = requests.put('https://cloud-api.yandex.net/v1/disk/resources/',
+                                params={'path': self.dir_path},
+                                headers={'Authorization': f'OAuth {self.yandex_token}'})
+            return resp.json()
+        else:
+            return 'Не указан путь'
+
+    def uploadYandex(self, file_path: str, photos, count=5):
+        result = []
+        for photo in zip(range(count), photos):
+
+            resp = requests.post('https://cloud-api.yandex.net/v1/disk/resources/upload',
+                                 params={'path': self.dir_path + str(photo[1]['name']),
+                                         'url': photo[1]['src']},
+                                 headers={'Authorization': f'OAuth {self.yandex_token}'})
+            if 'href' in resp.json():
+                result.append({"file_name": photo[1]['name'], "size": photo[1]['type']})
+            print("Загрузка фотографии "+str(photo[1]['name'])+" --- %s seconds ---" % (time.time() - start_time))
+        return result
+
+    def createJsonFile(self, data):
+        f = open('result.json', 'w', encoding="utf-8")
+        print("Созадние json файла --- %s seconds ---" % (time.time() - start_time))
+        json.dump(data, f)
+
+photoUploader = PhotoUpload(yandex_api_token, vk_api_token, vk_id)
+photos = photoUploader.getUserPhotos()
+photos = photoUploader.sortUserPhotos(photos)
+photoUploader.dir_path = '/124/'
+photoUploader.createYandexDir()
+jsonPhotos = photoUploader.uploadYandex(photoUploader.dir_path, photos, 2)
+photoUploader.createJsonFile(jsonPhotos)
